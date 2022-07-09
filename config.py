2c4d9b13da9a50511c099c923f4184dc7580800c
Lolofmeistahhz <99910272+Lolofmeistahhz@users.noreply.github.com>
2022-07-10 02:12:04 +0300

    remove some arguments

--- config.py
+++ config.py
@@ -4,21 +4,21 @@ from tkinter import  END
 default_font_size = 12
 default_font_family = "Consolas"
 
-def open_file(textbox: Text): # textbox это textbox
+def open_file(textbox): # textbox это textbox
     filepath = askopenfilename()
     with open(filepath, "r") as file:
         content = file.read()
     textbox.delete("1.0", END) # 1.0 line.column
     textbox.insert("1.0", content)
-def save_file(textbox: Text):
+def save_file(textbox):
     path = asksaveasfilename(filetypes=[('ALL Files', '*')])
     with open(path, "w") as file:
         file.write(textbox.get("1.0", END))
-def add_font_size(textbox: Text()):
+def add_font_size(textbox):
     global default_font_size
     default_font_size = default_font_size + 2
     textbox.config(font=(default_font_family, default_font_size))
-def minus_font_size(textbox: Text()):
+def minus_font_size(textbox):
     global default_font_size
     default_font_size = default_font_size - 2
     textbox.config(font=(default_font_family, default_font_size))
