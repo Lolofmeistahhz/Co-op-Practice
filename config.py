694f3bc3521e4baa6afb1448c30cff332f313619
Lolofmeistahhz <Lolofmeistahhz@gmail.com>
2022-07-10 01:49:54 +0300

    added global variable

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from tkinter.filedialog import asksaveasfilename, askopenfilename
 from tkinter.messagebox import *
 from tkinter import  END, Text
+global default_font_size
 default_font_size = 12
 default_font_family = "Consolas"
 
@@ -14,4 +15,9 @@ def save_file(textbox: Text):
     path = asksaveasfilename(filetypes=[('ALL Files', '*')])
     with open(path, "w") as file:
         file.write(textbox.get("1.0", END))
-        
+def add_font_size(textbox: Text()):
+    default_font_size = default_font_size + 2
+    textbox.config(font=(default_font_family, default_font_size))
+def minus_font_size(textbox: Text()):
+    default_font_size = default_font_size - 2
+    textbox.config(font=(default_font_family, default_font_size))
