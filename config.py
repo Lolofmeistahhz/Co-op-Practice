b18d773ab06efa30fddef16d269446006b886fca
Lolofmeistahhz <Lolofmeistahhz@gmail.com>
2022-07-10 00:39:00 +0300

    first upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from tkinter.filedialog import asksaveasfilename, askopenfilename
+from tkinter.messagebox import *
+from tkinter import  END, Text
+default_font_size = 12
+default_font_family = "Consolas"
+
+def open_file(textbox: Text): # textbox это textbox
+    filepath = askopenfilename()
+    with open(filepath, "r") as file:
+        content = file.read()
+    textbox.delete("1.0", END) # 1.0 line.column
+    textbox.insert("1.0", content)
+def save_file(textbox: Text):
+    path = asksaveasfilename(filetypes=[('ALL Files', '*')])
+    with open(path, "w") as file:
+        file.write(textbox.get("1.0", END))
+        
